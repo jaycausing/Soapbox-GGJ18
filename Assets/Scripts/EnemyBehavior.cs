96b7c0ffec5a389784faa3a8a1b9161861973a64
Jennifer Causing <jaycausing@jennifers-mbp.resource.campus.njit.edu>
2018-01-27 18:39:27 -0500

    created NavMesh and NavMeshAgent for world and base enemy

---
+++ Assets/Scripts/EnemyBehavior.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyBehavior : MonoBehaviour {
+
+	public Transform playerPos;
+
+	// Use this for initialization
+	void Start () {
+		NavMeshAgent agent = GetComponent<NavMeshAgent>();
+		agent.destination = playerPos;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
