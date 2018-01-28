92b5bc59527f6adcf8e26f510cd7922a7f7da2ce
Jennifer Causing <jaycausing@jennifers-mbp.resource.campus.njit.edu>
2018-01-27 22:16:10 -0500

    testing mic record levels

--- Assets/Scripts/ScreamInput.cs
+++ Assets/Scripts/ScreamInput.cs
@@ -4,25 +4,42 @@ using UnityEngine;
 
 public class ScreamInput : MonoBehaviour {
 
-	AudioSource aud;
+	AudioClip aud;
 	int minFreq = 0;
 	int maxFreq = 0;
 
+	void Awake(){
+		aud = Microphone.Start("Built-in Microphone", true, 5, 44100);
+	}
+
 	// Use this for initialization
 	void Start () {
-		aud = GetComponent<AudioSource>();
-		foreach (string device in Microphone.devices) {
-            Debug.Log("Name: " + device);
-        }
-		Microphone.GetDeviceCaps("Built-in Microphone", out minFreq, out maxFreq);
+		
 		Debug.Log("Device caps: " + minFreq.ToString() + ", " + maxFreq.ToString());
 
-		aud.clip = Microphone.Start("Built-in Microphone", true, 5, 44100);
+		
 		Debug.Log("Is the mic recording? " + Microphone.IsRecording("Built-in Microphone"));
+
+
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		
+		//get mic volume
+		int dec = 128;
+		float[] waveData = new float[dec];
+		int micPosition = Microphone.GetPosition(null)-(dec+1); // null means the first microphone
+		aud.GetData(waveData, micPosition);
+
+		// Getting a peak on the last 128 samples
+		float levelMax = 0;
+		for (int i = 0; i < dec; i++) {
+			float wavePeak = waveData[i] * waveData[i];
+			if (levelMax < wavePeak) {
+				levelMax = wavePeak;
+			}
+		}
+		float level = Mathf.Sqrt(Mathf.Sqrt(levelMax));
+		Debug.Log("Current input level: " + level);
 	}
 }
